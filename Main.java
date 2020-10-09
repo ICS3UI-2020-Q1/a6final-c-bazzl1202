653f06d70d06b1355b2a87d6b1a0f30348dc0d2d
bazzl1202 <bazzl1202@wrdsb.ca>
2020-10-09 12:27:10 +0000

    Puts 5 values into loop

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Creata black jack game that tells you count after 5 cards are drawn
+ * @author Liana Bazzarella
  */
 public class Main {
 
@@ -9,7 +10,22 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user to enter in 5 card values
+    System.out.println("Please enter the value of 5 cards:");
+
+    // create a constant for 5 spots in array
+    final int NUMB_CARDS = 5;
+    
+    // create an array
+    int[] cards = new int[NUMB_CARDS];
+
+    // put values into the array using a loop
+    for(int i = 0; i < NUMB_CARDS; i++){
+      cards[i]= input.nextInt();
+    }
     
   }
 }
